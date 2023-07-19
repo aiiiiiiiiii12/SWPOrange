db26d1d8d3d2f0dd3e03a3a06e1bf967d5fc959d
datpthe163028 <datpthe163028@fpt.edu.vn>
2023-07-19 20:23:27 +0700

    v1

--- ProjectS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProjectS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -84,7 +84,7 @@ namespace Project.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "{0} phải có {2} đến {1} kí tự", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -95,7 +95,7 @@ namespace Project.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Password và confirmation password không khớp")]
             public string ConfirmPassword { get; set; }
         }
 
